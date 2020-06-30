1733fa6690edf45ed36ef85e002e0b212647a126
Claire Kirby <claire.kirby@gmail.com>
2020-06-30 14:53:08 -0400

    added TenmoController and stubbed out methods

---
+++ TenmoServer/Controllers/TenmoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TenmoServer.Models;
+
+namespace TenmoServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TenmoController : ControllerBase
+    {
+
+        [HttpGet("users")]
+        public List<User> ListUsers()
+        {
+            return null;
+        }
+
+        [HttpGet("user/{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            return null;
+
+        }
+
+        [HttpGet("user/account/{id}")]
+        public ActionResult<User> GetAccountById(int id)
+        {
+            return null;
+
+        }
+        [HttpGet("transactions/{id}")]
+        public ActionResult<User> GetTransactionsByUserId(int id)
+        {
+            return null;
+
+        }
+
+        [HttpPost("transactions/new")]
+        public ActionResult<User> NewTransaction()
+        {
+            return null;
+
+        }
+
+        [HttpPut("transactions/{id}")]
+        public ActionResult<User> ApproveTransaction(int id)
+        {
+            return null;
+        }
+
+
+
+    }
+}
